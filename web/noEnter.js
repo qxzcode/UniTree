91af64b01c10ec198c484a245fbf02236481d884
Lori Liu <lyl2505@rit.edu>
2020-02-09 09:24:29 -0500

    Enter key doesn't work anymore, yay

---
+++ web/noEnter.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+        $("form").bind("keypress", function(e) {
+            if (e.keyCode == 13) {
+                return false;
+            }
+        });
+    });
